0c12e5df3b2029e8d52b087a88916b6683aaa279
feyzaozcini <feyza_ozcini@hotmail.com>
2024-07-26 10:31:13 +0300

    Services Edited

---
+++ task-management/userservice/src/main/java/com/turkcell/userservice/core/configuration/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.turkcell.userservice.core.configuration;
+
+import com.turkcell.tcellfinalcore.services.BaseSecurityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfiguration {
+    private final BaseSecurityService baseSecurityService;
+
+    private static final String[] WHITE_LIST={
+            "/api/v1/auth/**",
+            "/swagger-ui/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        baseSecurityService.configureCoreSecurity(http);
+        http
+                .authorizeHttpRequests((req)-> req
+                        .requestMatchers(WHITE_LIST).permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+
+}
